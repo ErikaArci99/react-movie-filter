33e1ffd5fb12f402a908889d06d950c8c56d5f98
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-13 23:56:25 +0200

    [UPD] ciclata e stilizzata la lista film visualizzata in pagina

--- react-movie-filter/src/App.jsx
+++ react-movie-filter/src/App.jsx
@@ -1,19 +1,36 @@
 import { useState } from 'react'
 import './App.css'
 
+const movies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+];
+
 function App() {
-  const movies = [
-    { title: 'Inception', genre: 'Fantascienza' },
-    { title: 'Il Padrino', genre: 'Thriller' },
-    { title: 'Titanic', genre: 'Romantico' },
-    { title: 'Batman', genre: 'Azione' },
-    { title: 'Interstellar', genre: 'Fantascienza' },
-    { title: 'Pulp Fiction', genre: 'Thriller' },
-  ];
 
   return (
     <>
-
+      <div className="container text-center">
+        <h1 className='mt-4'>Lista Film</h1>
+        <div className="row mt-4">
+          <div className="col-12">
+            <div className="col-12">
+              <ul className='list-group'>
+                {movies.map((movie, index) => {
+                  return (<li key={index} className="list-group-item">
+                    {movie.title} <em>{movie.genre}</em>
+                  </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
