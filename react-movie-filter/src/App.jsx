3f3b08a07d76bf3f5d268eef654e80d8b0e9ffec
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-13 23:45:20 +0200

    [UPD] inserito array film

--- react-movie-filter/src/App.jsx
+++ react-movie-filter/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const movies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ];
 
   return (
     <>
